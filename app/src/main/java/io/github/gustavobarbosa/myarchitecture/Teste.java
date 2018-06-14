dda3e403aa181ea93514cfa0daa4b85540046481
Gustavo Barbosa <gustavo.barreto@cedrotech.com>
2018-06-14 15:38:21 -0300

    primeiro commit

---
+++ app/src/main/java/io/github/gustavobarbosa/myarchitecture/Teste.java
@@ -0,0 +1,18 @@
+package io.github.gustavobarbosa.myarchitecture;
+
+public class Teste {
+    private String teste;
+
+    public String getTeste() {
+        return teste;
+    }
+
+    public void setTeste(String teste) {
+        this.teste = teste;
+    }
+
+    public Teste(String teste) {
+
+        this.teste = teste;
+    }
+}
